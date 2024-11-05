137f4ca54df694f1fc4c47340cdaf531e31a7394
日下陸矢 <rikuya.98@gmail.com>
2024-11-05 19:11:42 +0900

    ok

--- src/index.php
+++ src/index.php
@@ -1,81 +1,49 @@
 <?php
-// 変数
-//echo "Hello World!";
-//echo "<br>";
-//$num_1 = 123;
-//$num_2 = 324;
-//$num_string = $num_1 . $num_2;
-//$num_sum = $num_1 + $num_2;
-//var_dump($num_string);
-//echo "<br>";
-//var_dump($num_sum);
-//
-//
-//// 定数
-//const MAX = 100;
-//echo "<br>";
-//var_dump(MAX);
-//
-//// 配列
-//echo "<br>";
-//$array = [1, 2, 3, 4, 5];
-//var_dump($array);
-//echo "<br>";
-//
-//$array_2 = [
-//    ['orange', 'apple'],
-//    ['dog', 'cat']
-//];
-//
-//echo "<br>";
-//echo "<pre>";
-//var_dump($array_2);
-//echo "</pre>";
 
-// 連想配列
-$array_member = [
-    '田中' => [
-        'height' => 180,
-        'hobby' => '野球'
-    ],
-    '佐藤' => [
-        'height' => 170,
-        'hobby' => 'サッカー'
-    ]
-];
-echo '<br>';
-echo $array_member['田中']['height'];
-echo '<br>';
-echo $array_member['佐藤']['hobby'];
+// forの基本構文
+// break, continueも使える
 
-// 条件分岐
-$age = 20;
-$message = $age >= 20 ? '成人です' : '未成年です';
+echo 'forの基本構文';
 echo '<br>';
-echo $message;
-
-// 繰り返し
-$array_fruits = [
-    'apple' => [
-        'price' => 100,
-        'stock' => 10
-    ],
-    'banana' => [
-        'price' => 150,
-        'stock' => 5
-    ],
-    'orange' => [
-        'price' => 200,
-        'stock' => 8
-    ]
-];
-foreach($array_fruits as $key => $fruit) {
+for($i = 0; $i < 10; $i++) {
+    echo $i;
     echo '<br>';
-    echo $key . 'は';
-    echo '<br>';
-    echo $fruit['price']. '円で';
-    echo '<br>';
-    echo '個数'. $fruit['stock']. '個です';
 }
+
+// whileの基本構文
+echo 'whileの基本構文';
+echo '<br>';
+$i = 0;
+while ($i < 10) {
+    echo $i;
+    echo '<br>';
+    $i++;
+}
+
+//switchの基本構文
+echo 'switchの基本構文';
+echo '<br>';
+$signal = 'green';
+switch ($signal) {
+    case 'red':
+        echo 'stop';
+        break;
+    case 'blue':
+    case 'green':
+        echo 'go';
+        break;
+    case 'yellow':
+        echo 'caution';
+        break;
+    default:
+        echo 'wrong signal';
+        break;
+}
+
+/* switchの注意点
+    ・breakを書き忘れると、次のcaseも実行される
+    ・デフォルトでは比較演算子が==なので、型の比較も行いたい場合は、===を使う
+    ・上記のことから複雑になりがちなので、基本的にif文を使う
+*/
 
 ?>
