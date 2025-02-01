0c171af58773fe01d075e0c5b0b6d0d79e853ebb
日下陸矢 <rikuya.98@gmail.com>
2025-02-01 12:39:26 +0900

    FizzBuzzのユニットテストを作成

--- app/v1/Core/NumberConverter.php
+++ app/v1/Core/NumberConverter.php
@@ -5,6 +5,18 @@ class NumberConverter
 {
     public function convert(int $number): string
     {
-        return "";
+        if ($number % 3 === 0 && $number % 5 === 0) {
+            return 'FizzBuzz';
+        }
+
+        if ($number % 3 === 0) {
+            return 'Fizz';
+        }
+
+        if ($number % 5 === 0) {
+            return 'Buzz';
+        }
+
+        return (string) $number;
     }
 }
--- app/v1/Tests/Core/NumberConverterTest.php
+++ app/v1/Tests/Core/NumberConverterTest.php
@@ -11,5 +11,19 @@ class NumberConverterTest extends TestCase
     {
         $fizzBuzz = new NumberConverter();
         $this->assertEquals('1', $fizzBuzz->convert(1));
+
+        $this->assertEquals('2', $fizzBuzz->convert(2));
+
+        $this->assertEquals('Fizz', $fizzBuzz->convert(3));
+
+        $this->assertEquals('Buzz', $fizzBuzz->convert(5));
+
+        $this->assertEquals('Fizz', $fizzBuzz->convert(6));
+
+        $this->assertEquals('Buzz', $fizzBuzz->convert(10));
+
+        $this->assertEquals('FizzBuzz', $fizzBuzz->convert(15));
+
+        $this->assertEquals('FizzBuzz', $fizzBuzz->convert(30));
     }
 }
